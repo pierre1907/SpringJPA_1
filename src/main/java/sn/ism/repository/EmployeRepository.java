035f8e6c751e3d3dbd554b0fa19063c571b4dc23
pierre1907 <saint-pierre.kassi@ism.edu.sn>
2023-02-04 10:41:40 +0000

    update of febuary 4th

--- src/main/java/sn/ism/repository/EmployeRepository.java
+++ src/main/java/sn/ism/repository/EmployeRepository.java
@@ -4,6 +4,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import sn.ism.domaine.Employe;
 
-public interface EmployeRepository extends JpaRepository<Employe,Integer> {
+import java.util.List;
+
+public interface EmployeRepository extends JpaRepository<Employe,Integer>
+{
+    List<Employe> findByNom(String nom);
+    List<Employe> findByPrenom(String prenom);
+    List<Employe> findByNomAndPrenom(String nom, String prenom);
+    List<Employe> findBySalaire(Integer salaire);
+    List<Employe> findBySalaireGreaterThan(Integer salaire);
+    List<Employe> findBySalaireLessThan(Integer salaire);
+    List<Employe> findBySalaireBetween(Integer salaire1, Integer Salaire2);
+    List<Employe> findByNomContaining(String prototype);
+    List<Employe> findByNomLike(String prototype);
+
+
+
 
 }
